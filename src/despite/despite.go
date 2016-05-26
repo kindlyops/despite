8534973c427229e5e336f1c6b18de093c30b9930
Elliot Murphy <statik@users.noreply.github.com>
2016-05-26 16:46:00 -0400

    Add a failing test.

--- src/despite/despite.go
+++ src/despite/despite.go
@@ -44,10 +44,10 @@ func tableSize(ctx *cli.Context) error {
     pg_size_pretty(pg_table_size(c.oid)) AS table_size,
     pg_size_pretty(pg_indexes_size(c.oid)) AS index_size
   FROM pg_class c
-  LEFT JOIN pg_namespace n ON (n.oid = c.relnamespace)
+    LEFT JOIN pg_namespace n ON (n.oid = c.relnamespace)
   WHERE n.nspname NOT IN ('pg_catalog', 'information_schema')
-  AND n.nspname !~ '^pg_toast'
-  AND c.relkind='r'
+    AND n.nspname !~ '^pg_toast'
+    AND c.relkind='r'
   ORDER BY pg_total_relation_size(c.oid) DESC`
 	rows, err := db.Query(sql)
 	if err != nil {
---
+++ src/despite/despite_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestTableSizes(t *testing.T) {
+	// do something
+	if true {
+		t.Errorf(`The test failed in that way`)
+	}
+}
