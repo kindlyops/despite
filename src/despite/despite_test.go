ec6ac3d3dd30f3e6e9d95390745c4a6a6b35d61f
Elliot Murphy <statik@users.noreply.github.com>
2016-05-26 21:55:50 -0400

    Create a test table to see size in test output.

--- src/despite/despite_test.go
+++ src/despite/despite_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
 	"fmt"
 	"strings"
 	"testing"
@@ -11,20 +12,26 @@ func TestTableSizesCommand(t *testing.T) {
 	saved := dburi
 	dburi = "postgres://localhost/postgres?sslmode=disable"
 	var buf bytes.Buffer
-	// TODO set up some tables to get sizes from
-	err := tableSize(&buf)
+	db, err := sql.Open("postgres", dburi)
+	if err != nil {
+		t.Errorf(fmt.Sprintf("Got error %s", err))
+	}
+	defer db.Close()
+	_, err = db.Exec("CREATE TEMP TABLE testdata (d jsonb)")
+	err = tableSize(&buf)
 	dburi = saved
 	if err != nil {
 		t.Errorf(fmt.Sprintf("Got error %s", err))
 	}
 	raw := []string{
-		"  NAME | TOTALSIZE | TABLESIZE | INDEXSIZE  ",
-		"+------+-----------+-----------+-----------+\n",
+		"    NAME   | TOTALSIZE  | TABLESIZE  | INDEXSIZE  ",
+		"+----------+------------+------------+-----------+",
+		"  testdata | 8192 bytes | 8192 bytes | 0 bytes    \n",
 	}
 	expected := strings.Join(raw, "\n")
 
 	if buf.String() != expected {
-		f2 := "table-size output is:\n%q\nexpected:\n%q"
+		f2 := "table-size output is:\n%s\nexpected:\n%s"
 		t.Errorf(f2, buf.String(), expected)
 	}
 }
